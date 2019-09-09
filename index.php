7e2d9f60846d50b1d84e782e8db60b1ebd6c0b7e
Kamakepar2028 <54079524+Kamakepar2028@users.noreply.github.com>
2019-09-09 13:11:02 +0300

    Index
    Php

---
+++ index.php
@@ -0,0 +1,10 @@
+$userid = $_GET['userid']; 
+$pwd = $_GET['pwd']; 
+echo "$userid and $pwd ,Hello"; 
+if ($userid = "Kama+Kepar") and ($pwd = "kirik1028") 
+{ 
+echo "$userid and $pwd ,Hello"; 
+} 
+else 
+{ 
+echo "Something went wrong";
